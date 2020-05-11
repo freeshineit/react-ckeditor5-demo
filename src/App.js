0189f9e92e6bef30cb353afdf64e89a5af8f09f4
shine <xiaoshaoqq@gmail.com>
2020-05-11 09:37:39 +0800

    config

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import "@ckeditor/ckeditor5-build-classic/build/translations/zh-cn"; // 语言包
 
 import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
@@ -31,6 +32,20 @@ import Font from "@ckeditor/ckeditor5-font/src/font";
 import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
 import Highlight from "@ckeditor/ckeditor5-highlight/src/highlight";
 
+const fontSizeConfig = {
+  options: [
+    {
+      title: "Small",
+      model: "8px",
+    },
+    "default",
+    {
+      title: "Big",
+      model: "14px",
+    },
+  ],
+};
+// https://ckeditor.com/docs/ckeditor5/latest/api/module_core_editor_editorconfig-EditorConfig.html#member-alignment
 const editorConfiguration = {
   plugins: [
     Essentials,
@@ -83,6 +98,19 @@ const editorConfiguration = {
       "redo",
     ],
   },
+  fontSize: fontSizeConfig,
+  fontFamily: {
+    options: [
+      "宋体",
+      "仿宋",
+      "微软雅黑",
+      "黑体",
+      "仿宋_GB2312",
+      "楷体",
+      "隶书",
+      "幼圆",
+    ],
+  },
   image: {
     toolbar: [
       "imageStyle:full",
@@ -94,8 +122,39 @@ const editorConfiguration = {
   table: {
     contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
   },
+  placeholder: "请输入文章...",
   // This value must be kept in sync with the language defined in webpack.config.js.
   language: "zh-cn",
+  heading: {
+    // 設定 Heading 內的樣式，可新增多個
+    options: [
+      {
+        model: "paragraph",
+        title: "Paragraph",
+        class: "ck-heading_paragraph",
+      },
+      {
+        model: "heading1",
+        view: "h2",
+        title: "H2",
+        class: "ck-heading_heading1",
+      },
+      {
+        model: "heading2",
+        view: "H3",
+        title: "Heading 2",
+        class: "ck-heading_heading2",
+      },
+    ],
+  },
+  ckfinder: {
+    // 后端上传图片API 路径
+    uploadUrl: `/upload`,
+    options: {
+      // 限定类型为图片
+      resourceType: "Images",
+    },
+  },
 };
 
 class App extends Component {
@@ -128,51 +187,3 @@ class App extends Component {
 }
 
 export default App;
-
-// import React, { Component } from "react";
-
-// import CKEditor from "@ckeditor/ckeditor5-react";
-
-// // NOTE: Use the editor from source (not a build)!
-// import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
-
-// import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-// import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
-// import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-// import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
-
-// const editorConfiguration = {
-//   plugins: [Essentials, Bold, Italic, Paragraph],
-//   toolbar: ["bold", "italic"],
-// };
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <h2>Using CKEditor 5 from source in React</h2>
-//         <CKEditor
-//           editor={ClassicEditor}
-//           config={editorConfiguration}
-//           data="<p>Hello from CKEditor 5!</p>"
-//           onInit={(editor) => {
-//             // You can store the "editor" and use when it is needed.
-//             console.log("Editor is ready to use!", editor);
-//           }}
-//           onChange={(event, editor) => {
-//             const data = editor.getData();
-//             console.log({ event, editor, data });
-//           }}
-//           onBlur={(event, editor) => {
-//             console.log("Blur.", editor);
-//           }}
-//           onFocus={(event, editor) => {
-//             console.log("Focus.", editor);
-//           }}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
